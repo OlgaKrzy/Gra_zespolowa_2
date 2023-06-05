dfadb622d28f6a75fc62502c44e9c84f3f9920a3
Agata Rosada <75192@student.ue.poznan.pl>
2023-06-05 13:00:26 +0200

    dodatkowe zmiany

--- main.py
+++ main.py
@@ -36,13 +36,14 @@ def porzadkuj_do_lewej(input_data):
     for row in input_data:
         formatted_row = [str(row[i]).ljust(column_widths[i]) for i in range(num_columns)]
         formatted_rows.append(formatted_row)
-    
-    output = '['
+
+
     for row in formatted_rows:
-        output += '[' + ', '.join(row) + '],\n'
-    output = output.rstrip(',\n') + ']'
+        output = '[' + '],\n['.join(', '.join(row) for row in formatted_rows) + ']'
     return output
 
 input_data = [[1, 2, 10, 150], [10, 2, 1000, 2], [1, 120, 1, 1000]]
 output = porzadkuj_do_lewej(input_data)
 print(output)
+
+assert porzadkuj_do_lewej([[1, 2, 10, 150], [10, 2, 1000, 2], [1, 120, 1, 1000]]) == '[[1 , 2  , 10  , 150 ],/n [10, 2  , 1000, 2   ],/n [1 , 120, 1   , 1000]]'
